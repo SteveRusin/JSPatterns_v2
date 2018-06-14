0dba310bb5ead42adda615e29f06a873877b58c2
Steve <steve.neeson21@gmail.com>
2018-06-14 22:38:09 +0300

    done singleton

--- main.js
+++ main.js
@@ -1,32 +1,25 @@
-function Observer() {
-    const subscribers = [];
+const Singleton =  (function() {
+    let instance;
 
-    this.subscribe = newSubscriber => subscribers.push(newSubscriber);
-    this.unsubscribe = targetSubscriber => {
-        const indexOfTarget = subscribers.indexOf(targetSubscriber);
-        if(~indexOfTarget) {
-            subscribers.splice(indexOfTarget, 1);
-        } else {
-            console.warn(`Subscriber ${targetSubscriber} not found`);
-        }
+   const returnInstance = () => {
+        instance = new Object();
+        Object.freeze(instance);
+        return instance;
     }
 
-    this.notify = (data, context = window) => {
-        subscribers.forEach((subscriber)=>{
-            subscriber.call(context, data);
-        })
+
+
+    const getInstance = () => instance
+                                    ? (console.log(`Instance exist`), instance)
+                                    : (console.log(`No Instance`), returnInstance());
+
+    return {
+        getInstance
     }
-}
+                                    
+}());
 
-function eventHandler(data) {
-    console.log(`Data passed to subscribers: ${data}`)
-}
 
-const observable = new Observer();
-
-observable.subscribe(eventHandler);
-observable.notify('item 1');
-observable.unsubscribe(eventHandler);
-observable.notify('item 2');
-observable.subscribe(eventHandler);
-observable.notify('item 3');
+Singleton.getInstance();
+Singleton.getInstance();
+Singleton.getInstance();
