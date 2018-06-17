f88226932d14e5d73e707266befc54dedcc9b3d1
Steve <steve.neeson21@gmail.com>
2018-06-17 22:05:14 +0300

    done mediator

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
-function Iterator(items) {
-    const _items = items;
-    let index = 0;
+function Mediator() {
+    const topics = {};
 
-    this.first = () => _items[0];
-    this.next = () => (index++, _items[index]);
-    this.current = () => _items[index];
-    this.reset = () => index = 0;
+    this.subscribe = function (topic, fn) {
+        if(!topics[topic]) {
+            topics[topic] = [];
+        }
+
+        topics[topic].push({
+            context: this,
+            callback: fn
+        })
+
+        return this;
+    }
+
+    this.publish = function(topic) {
+        let args;
+
+        if(!topic[topic]) {
+            return false;
+        }
+
+        args = Array.prototype.slice.call(arguments, 1);
+
+        for (let i = 0, l = topics[topic].length; i < l; i++) {
+            const subscription = topics[topic][i];
+            subscription.callback.apply(subscription.context, args);
+        }
+
+        return this;
+    }
+
+    this.installTo = function (obj) {
+        obj.subscribe = this.subscribe;
+        obj.publish = this.publish;
+    }
 }
